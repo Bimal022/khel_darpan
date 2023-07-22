08ef2c75442ccf1e50b52e9939dbc61300017399
Kumar Bimal <pk368129@gmail.com>
2023-07-22 09:26:41 +0530

    Calendar

--- lib/screens/CalenderPage.dart
+++ lib/screens/CalenderPage.dart
@@ -1,12 +1,75 @@
 import 'package:flutter/material.dart';
 
+import '../Components/Constants/constants.dart';
+import '../Components/UpcomingMatches.dart';
+
 class CalenderPage extends StatelessWidget {
   const CalenderPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: const Text("This Is for calender page"),
-    );
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              child: Text(
+                "Calendar",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Container(
+              height: 300,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    UpcomingEvents(
+                      teamName1: "India",
+                      flagPath1: "assets/flags/india.png",
+                      teamName2: "Australia",
+                      flagPath2: "assets/flags/aus.png",
+                    ),
+                    UpcomingEvents(
+                      teamName1: "China",
+                      flagPath1: "assets/flags/china.png",
+                      teamName2: "France",
+                      flagPath2: "assets/flags/france.png",
+                    ),
+                    UpcomingEvents(
+                      teamName1: "Germany",
+                      flagPath1: "assets/flags/germany.png",
+                      teamName2: "Australia",
+                      flagPath2: "assets/flags/greatbritain.png",
+                    ),
+                    UpcomingEvents(
+                      teamName1: "Italy",
+                      flagPath1: "assets/flags/italy.png",
+                      teamName2: "Australia",
+                      flagPath2: "assets/flags/aus.png",
+                    ),
+                    UpcomingEvents(
+                      teamName1: "India",
+                      flagPath1: "assets/flags/india.png",
+                      teamName2: "Japan",
+                      flagPath2: "assets/flags/japan.png",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
